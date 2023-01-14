27161f96353e2e553cfc7f0a047541ea42eee4da
AlgoAngel <laoroney1113@gmail.com>
2023-01-14 12:40:48 -0300

    fix: sequelize module

--- apps/api/db/mongo/index.js
+++ apps/api/db/mongo/index.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import {config} from '../config.js'
+import {config} from '../../config.js'
 // mongoose.Promise = global.Promise
 async function dbConnect() {
     try {
