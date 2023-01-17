46f5e90a8f2c2eb16c044ecf2f52c62dab05dffe
AlgoAngel <laoroney1113@gmail.com>
2023-01-17 16:00:30 -0300

    changin env variable

--- apps/api/config.js
+++ apps/api/config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 export const config = {
-    MONGO_URL:process.env.MONGO_URL ?? "mongodb://root:example@localhost:27017",
+    MONGO_URI:process.env.MONGO_URI ?? "mongodb://root:example@localhost:27017",
     PORT: process.env.PORT ?? 3000,
     HOST: process.env.HOST ?? "http://localhost"
     
