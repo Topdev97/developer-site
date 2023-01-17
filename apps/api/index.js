27cdadf2a8e053a3ef729d3c221b32493785f870
AlgoAngel <laoroney1113@gmail.com>
2023-01-17 11:24:15 -0300

    deleting firebase auth

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,6 +1,4 @@
 import "dotenv/config";
-import { getAuth } from "firebase-admin/auth";
-import { authenticate } from "./middlewares/auth.handler.js";
 import { app } from "./app.js";
 import { dbConnect } from "./db/mongo/index.js";
 import { config } from "./config.js";
@@ -8,11 +6,3 @@ const {PORT} = config
 app.listen(PORT, () => {
   console.log(`[express]: Escuchando en el puerto ${PORT}`);
 });
-// getAuth()
-//   .getUserByEmail("davc93@gmail.com")
-//   .then((value) => {
-//     console.log(value);
-//   })
-//   .catch((error)=>{
-//     console.error(`[Error-auth]:${error}`)
-//   })
