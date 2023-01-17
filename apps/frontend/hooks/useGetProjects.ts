233787bd40f236ca97238adaa273a7356866b055
AlgoAngel <laoroney1113@gmail.com>
2023-01-16 22:13:21 -0300

    re factor hooks of frontend

--- apps/frontend/hooks/useGetProjects.ts
+++ apps/frontend/hooks/useGetProjects.ts
@@ -1,17 +1,31 @@
 import React from "react";
-
+import {Project} from '../models/project.model'
 export const useGetProjects = () => {
-    const [projects, setProjects] = React.useState([])
+    const [projects, setProjects] = React.useState<Project[]>([])
+    const [loading, setLoading] = React.useState<Boolean>(false)
+    const [error, setError] = React.useState<String | null>(null)
+
     React.useEffect(()=>{
         const getProductList =async () => {
-            const response = await fetch('/api/projects')
-            const data = await response.json()
-            setProjects(data)
+            setLoading(true)
+            try {
+                const response = await fetch('/api/projects')
+                const data = await response.json()
+                setProjects(data)
+                setLoading(false)                
+            } catch (error) {
+                setError(error as string)
+                setLoading(false)
+                
+            }
+
         }
         getProductList()
     },[])
 
     return {
-        projects
+        projects,
+        loading,
+        error
     }
 };
