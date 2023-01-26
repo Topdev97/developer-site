c997782dc26d0a005d7bc00ece6f92e742d656ff
AlgoAngel <laoroney1113@gmail.com>
2023-01-26 11:26:17 -0300

    fix: settimeout

--- apps/client/web/hooks/useGetProjects.ts
+++ apps/client/web/hooks/useGetProjects.ts
@@ -1,34 +1,30 @@
 import React from "react";
-import {Project} from '../models/project.model'
+import { Project } from "../models/project.model";
 export const useGetProjects = () => {
-    const [projects, setProjects] = React.useState<Project[]>([])
-    const [loading, setLoading] = React.useState<Boolean>(false)
-    const [error, setError] = React.useState<String | null>(null)
+  const [projects, setProjects] = React.useState<Project[]>([]);
+  const [loading, setLoading] = React.useState<Boolean>(false);
+  const [error, setError] = React.useState<String | null>(null);
 
-    React.useEffect(()=>{
-        const getProductList =async () => {
-            setLoading(true)
-            setTimeout(async ()=>{
+  React.useEffect(() => {
+    const getProductList = async () => {
+      setLoading(true);
 
-                try {
-                    const response = await fetch('/api/projects')
-                    const data = await response.json()
-                    setProjects(data)
-                    setLoading(false)                
-                } catch (error) {
-                    setError(error as string)
-                    setLoading(false)
-                    
-                }
-            },6000)
+      try {
+        const response = await fetch("/api/projects");
+        const data = await response.json();
+        setProjects(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error as string);
+        setLoading(false);
+      }
+    };
+    getProductList();
+  }, []);
 
-        }
-        getProductList()
-    },[])
-
-    return {
-        projects,
-        loading,
-        error
-    }
+  return {
+    projects,
+    loading,
+    error,
+  };
 };
