f88bffd76ca40cc49ab869c690a8e83813b48d68
AlgoAngel <laoroney1113@gmail.com>
2023-02-03 21:27:44 -0300

    adding cv url

--- apps/client/web/components/CV/index.tsx
+++ apps/client/web/components/CV/index.tsx
@@ -2,8 +2,7 @@
 
 
 export const CV = (props:any) => {
-    const uri = `https://drive.google.com/file/d/125RELWGNV0HRzPZjkRPf0LHGPFcj6yM6/view?usp=sharing`
-    
+    const uri = process.env.NEXT_PUBLIC_CV_URI
     
     return (
     <a href={uri} download className={`text-lg text-center font-semibold bg-primary-400 hover:bg-primary-500 transition-all text-primary-100 px-4 py-3 w-full md:w-72 lg:mt-0 ${props.styles}`}>Download CV</a>
