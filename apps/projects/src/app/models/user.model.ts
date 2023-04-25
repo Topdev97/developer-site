1beb95a9e32b4a0323a081c93d3911ef0e1b1bee
AlgoAngel <laoroney1113@gmail.com>
2023-04-25 12:42:54 -0400

    adding nextjs

--- apps/projects/src/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id:       string;
-    email:    string;
-    role:       string;
-    password?: string;
-}
--- apps/projects/src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  apiUrl:"https://davc93-production.up.railway.app/v1"
-};
