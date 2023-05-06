81eeedc42d8ccb13c0b21588ba476b361aaa7946
AlgoAngel <laoroney1113@gmail.com>
2023-05-06 17:58:05 -0400

    se anade markdown format y se ajustan estilos nuevamente

--- apps/projects/src/components/ProjectDetail/index.tsx
+++ apps/projects/src/components/ProjectDetail/index.tsx
@@ -1,7 +1,8 @@
 import { Project } from '@/models/project.model'
+import Markdown from 'markdown-to-jsx'
 import Link from 'next/link'
 import React from 'react'
-
+Markdown
 
 type ProjectDetailProps = {
     project:Project
@@ -13,9 +14,13 @@ export const ProjectDetail = ({project}:ProjectDetailProps) => {
     <div className='flex flex-col gap-5 md:gap-10'>
         <h1>{project.title}</h1>
         <div className='flex flex-col gap-10 lg:flex-row lg:justify-between'>
-            <div className='mt-16 project-description max-w-lg'>{project.description}</div>
+            <div className='mt-16 project-description max-w-lg'>
+                <Markdown >
+                    {project.description}
+                </Markdown>
+                </div>
 
-        <div className='images-container mt-16 lg:mr-20 w-72 h-72 overflow-hidden container max-w-sm'>
+        <div style={{alignSelf:'center'}} className='images-container my-8 mt-16 lg:mr-20 w-72 h-72 overflow-y-scroll overflow-x-hidden container max-w-sm'>
         {project.images.map((image)=>{
             return (
                 <img key={image.id} src={image.url} alt={`${image.id}`} />
@@ -25,12 +30,12 @@ export const ProjectDetail = ({project}:ProjectDetailProps) => {
         </div>
         </div>
         
-        <h3>Technologies</h3>
-        <div className='labels container grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-5 '>
+        <h3 style={{alignSelf:'center'}}>Technologies</h3>
+        <div className='labels container mt-4 mb-8 grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 md:gap-7 '>
             {project.labels.map((label)=>{
                 return (
                     <div key={label.id} className='flex flex-col items-center'>
-                        <img className='h-14 md:h-20' src={label.image} alt="" />
+                        <img className='h-12 md:h-20' src={label.image} alt="" />
                         <span>{label.title}</span>
                     </div>
                 )
@@ -40,7 +45,7 @@ export const ProjectDetail = ({project}:ProjectDetailProps) => {
             <Link href={project.repository} className='btn--primary w-64'>
                 Go to Repo
             </Link>
-            <Link href={project.link} className='btn--secondary w-64'>
+            <Link href={project.link} className='btn--tertiary w-64'>
                 Go to App
             </Link>
         </div>
--- apps/projects/src/components/ProjectItem/index.tsx
+++ apps/projects/src/components/ProjectItem/index.tsx
@@ -19,7 +19,7 @@ export const ProjectItem = ({project}:ProjectItemProps) => {
     <div className='flex flex-col items-center gap-4 py-4 px-2'>
         {project.published && <span>Icono</span>}
         <img src={project.images[0].url} alt={project.title} className='h-32 mb-4' />
-        <h3>{project.title}</h3>
+        <h4>{project.title}</h4>
         <p style={{whiteSpace:'pre-wrap',WebkitBoxOrient:'vertical',WebkitLineClamp:2,overflow:'hidden',display:'-webkit-box',marginBottom:20}}>{project.shortDescription}</p>
         <Link className='btn--primary flex justify-center w-48' href={`/${project.slug}`} onClick={handleClick}>
           {loading ? <ButtonLoader/> : 'Go to Details'}  
