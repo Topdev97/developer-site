bfbe76f5874ce0962df3012f545e0ea96d12a858
AlgoAngel <laoroney1113@gmail.com>
2023-05-08 14:41:12 -0400

    se hacen barras de busqueda para labels y projects

--- apps/admin/src/components/ListOfLabels/index.tsx
+++ apps/admin/src/components/ListOfLabels/index.tsx
@@ -1,10 +1,15 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  FormEventHandler,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { labelService } from "../../services/label.service";
 import { Label } from "../../models/label.model";
 
 import "./style.css";
 import { LabelItem } from "../LabelItem";
-
+import { useInputValue } from "../../hooks/useInputValue";
 
 export const useGetLabels = () => {
   const [loading, setLoading] = useState(false);
@@ -28,34 +33,104 @@ export const useGetLabels = () => {
     loading,
     error,
     labels,
-    getLabels
-  }
-}
+    getLabels,
+  };
+};
 
 export const ListOfLabels = () => {
-  const {loading,error,labels,getLabels} = useGetLabels()
-  const [filteredLabels, setFilteredLabels] = useState<Label[]>(labels)
+  const { loading, error, labels, getLabels } = useGetLabels();
+  const [filteredLabels, setFilteredLabels] = useState<Label[]>(labels);
+  const searchInput = useInputValue("");
+  const [isOpenFilter, setIsOpenFilter] = useState(false);
+
+  const handleShowFilter = () => {
+    setIsOpenFilter(!isOpenFilter);
+  };
   // End Hooks
   useEffect(() => {
-    setFilteredLabels(labels)
-  
-  }, [labels])
-  
+    setFilteredLabels(labels);
+  }, [labels]);
+  useEffect(() => {
+    const filter = labels.filter((label) =>
+      label.title.toLowerCase().includes(searchInput.value.toLowerCase())
+    );
+    setFilteredLabels(filter);
+  }, [searchInput.value]);
 
   return (
-    <div className="labels-list">
-      {loading && <p>Loading</p>}
-      {error && <p>{error}</p>}
-
-      <div className="labels-list__item">
-        <h5>Id</h5>
-        <h5>Title</h5>
-        <h5>Type</h5>
-        <h5>Image</h5>
-        <h5>Created At</h5>
-        <h5>Options</h5>
+    <section className="label-list lg:px-20">
+      <div className="label-list__options my-4">
+        <form className="label-list__searchbar">
+          <input
+            type="text"
+            placeholder="write a label name"
+            {...searchInput}
+          />
+        </form>
       </div>
-      {filteredLabels.map((label) => <LabelItem key={label.id} label={label} getLabels={getLabels} filteredLabels={filteredLabels} setFilteredLabels={setFilteredLabels} /> )}
-    </div>
+      <div className="label-list__headers">
+        <div>
+          <h5>Id</h5>
+        </div>
+        <div>
+          <h5>Title</h5>
+        </div>
+        <div className="flex relative gap-2">
+          <h5>Type</h5>
+          <div className="absolute" style={{ right: -35 }}>
+            <button onClick={handleShowFilter}>
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4C3.44772 8 3 7.55228 3 7ZM6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12ZM9 17C9 16.4477 9.44772 16 10 16H14C14.5523 16 15 16.4477 15 17C15 17.5523 14.5523 18 14 18H10C9.44772 18 9 17.5523 9 17Z"
+                  fill="#000000"
+                />
+              </svg>
+            </button>
+            {isOpenFilter && (
+              <div className="bg-white p-3 absolute">
+                <li className="flex justify-end gap-2">
+                  <p>text</p>
+                  <input type="checkbox" name="" id="" placeholder="text" />
+                </li>
+                <li className="flex">
+                  <p>other</p>
+                  <input type="checkbox" name="" id="" placeholder="other" />
+                </li>
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <h5>Image</h5>
+        </div>
+        <div>
+          <h5>Created At</h5>
+        </div>
+        <div>
+          <h5>Options</h5>
+        </div>
+      </div>
+      <div className="label-list__items">
+        {loading && <p>Loading</p>}
+        {error && <p>{error}</p>}
+        {filteredLabels.map((label) => (
+          <LabelItem
+            key={label.id}
+            label={label}
+            getLabels={getLabels}
+            filteredLabels={filteredLabels}
+            setFilteredLabels={setFilteredLabels}
+          />
+        ))}
+      </div>
+    </section>
   );
 };
--- apps/admin/src/components/ListOfProjects/index.tsx
+++ apps/admin/src/components/ListOfProjects/index.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Project } from "../../models/project.model";
 import { projectService } from "../../services/project.service";
 import "./style.css";
 import { ProjectItem } from "../ProjectItem";
+import { useInputValue } from "../../hooks/useInputValue";
 
 export const useGetProjects = () => {
   const [projects, setProjects] = React.useState<Project[]>([]);
@@ -28,14 +29,34 @@ export const useGetProjects = () => {
     projects,
     loading,
     error,
-    getProjects
-  }
-}
-
+    getProjects,
+  };
+};
 export const ListOfProjects = () => {
-  const {projects,loading,error} = useGetProjects()
+  const { projects, loading, error } = useGetProjects();
+  const searchInput = useInputValue("");
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([])
+  useEffect(() => {
+    const filter = projects.filter((project) =>
+      project.title.toLowerCase().includes(searchInput.value.toLowerCase())
+    );
+    setFilteredProjects(filter);
+  }, [searchInput.value]);
+  useEffect(()=>{
+    setFilteredProjects(projects)
+  },[projects])
   return (
-    <>
+    <section className="lg:px-20">
+
+      <div className="project-list__options my-4">
+        <form className="project-list__searchbar">
+          <input
+            type="text"
+            placeholder="write a label name"
+            {...searchInput}
+          />
+        </form>
+      </div>
       <div className="project-list__headers">
         <h5>Id</h5>
         <h5>Title</h5>
@@ -47,13 +68,14 @@ export const ListOfProjects = () => {
         <h5>Created At</h5>
         <h5>Options</h5>
       </div>
-      <div className="grid gap-2 mt-3">
-
-        {projects.map((project) => <ProjectItem project={project}/>)}
+      <div className="project-list__items grid gap-2 mt-3">
+        {filteredProjects.map((project) => (
+          <ProjectItem project={project} />
+        ))}
       </div>
 
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-    </>
+    </section>
   );
 };
