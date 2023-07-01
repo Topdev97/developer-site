956534c4cf692e70a1d3c74181c1cc223666ca85
AlgoAngel <laoroney1113@gmail.com>
2023-07-01 17:30:42 -0400

    se cambia gestor de monorepo a npm y se elimina storybook momentaneamente

--- apps/frontend/storybook/storybook-static/assets/Button-c0222589.js
+++
@@ -1,2 +0,0 @@
-const r=({primary:o=!1,size:c="medium",backgroundColor:e,label:s,onClick:n})=>{const t=document.createElement("button");t.type="button",t.innerText=s,n&&t.addEventListener("click",n);const u=o?"button--primary":"button--secondary";return t.className=["button",`button--${c}`,u].join(" "),e&&(t.style.backgroundColor=e),t};export{r as c};
-//# sourceMappingURL=Button-c0222589.js.map
--- apps/frontend/storybook/storybook-static/assets/syntaxhighlighter-QTQ2UBB4-0b4d8d40.js
+++
@@ -1,2 +0,0 @@
-import{S as c,c as l,s as n}from"./index-7cfc88ce.js";import"./iframe-e64fa401.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-d38538b0.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-0b4d8d40.js.map
