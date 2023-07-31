3a626dc22adc1c99dc7ab8da976ef6949019f310
AlgoAngel <laoroney1113@gmail.com>
2023-07-31 18:47:48 -0400

    se actualiza storybook

--- apps/workshop-html/.storybook/main.ts
+++ apps/workshop-html/.storybook/main.ts
@@ -1,17 +1,21 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/html-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm")],
   framework: {
-    name: "@storybook/html-vite",
-    options: {},
+    name: getAbsolutePath("@storybook/html-vite"),
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
