c2c35eabedeb219d212dd084d8e97ae8a1bd3b21
AlgoAngel <laoroney1113@gmail.com>
2023-08-23 19:36:58 -0400

    se instala react workshop

--- apps/workshop/src/stories/Button.stories.ts
+++ apps/workshop/src/stories/Button.stories.ts
@@ -1,11 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from 'ui';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
@@ -40,4 +47,12 @@ export const Small: Story = {
     size: 'small',
     label: 'Button',
   },
+};
+
+export const Warning: Story = {
+  args: {
+    primary: true,
+    label: 'Delete now',
+    backgroundColor: 'red',
+  }
 };
