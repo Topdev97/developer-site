44cbebd2fadfb7fb6416574ff2a5d6e6495cb955
AlgoAngel <laoroney1113@gmail.com>
2023-08-25 21:19:34 -0400

    exporting only necessary

--- packages/ui-html/index.ts
+++ packages/ui-html/index.ts
@@ -1,13 +1,8 @@
 export * from "./Button";
-export * from "./Header";
-// export * from "./Markdown"
-export * from "./Card"
 export * from "./Typography"
-export * from "./"
 export * from "./NavbarDesktop"
 export * from './NavbarMobile'
-export * from './Slider'
 export * from "./Modal"
 export * from "./TextField"
-
 export * from "./TextArea"
+// export * from "./Card"
